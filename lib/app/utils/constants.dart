ec7b024e17e1b29cee888c681572a6e55c5a764e
mcifapps <mcif.apps@gmail.com>
2024-10-03 16:00:51 +0200

    switch prod environment

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -3,5 +3,5 @@ const apiKey        = '3f834ef77eaddfcaad9e5955dc3a6feb';
 const imageBaseUrl  = 'https://image.tmdb.org/t/p/original';
 
 const URL_HOST      = "192.168.1.13";
-const URL_PREFIX    = "/v63_mcif/phpfa";
+const URL_PREFIX    = "/v70_mcif/phpfa";
 const URL_SCHEME    = "http";
